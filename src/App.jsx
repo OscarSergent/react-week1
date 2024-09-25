8719bff0a8177a243147cb0feecf0cef69817969
Isnitho <user@Isnitho.com>
2024-09-25 09:44:01 +0200

    exo props

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import './App.css'
 import User from './User'
 
 function App() {
-
-  const info = {
-    name  : 'John',
-    age   : 32
-  }
+  const infos = [
+    {
+      name  : 'John',
+      age   : 32,
+      url   : 'https://placehold.co/200'
+    },
+    {
+      name  : 'Jane',
+      age   : 25,
+      url   : 'https://placehold.co/200'
+    },
+    {
+      name  : 'Jem',
+      age   : 42,
+      url   : 'https://placehold.co/200'
+    },
+  ]
 
   return (
     <>
-      <User {...info}/>
+      {infos.map((info, i) => <User {...info} key={i}/>)}
     </>
   )
 }
--- src/User.jsx
+++ src/User.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable react/prop-types */
 
 
 
-const User = ({name, age}) => {
+const User = ({name, age, url}) => {
     
 
     return (
         <>
+            <img style={{height:'200px', width:'200px'}} src={url} />
             <h1>Hello my name is : { name }, I am { age }</h1>
         </>
     )
